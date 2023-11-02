e4178df88df5dd457e7506f1bdfcb82b00f575b4
Arespi <arespigames@gmail.com>
2023-11-02 18:02:31 +0800

    Implement Snap to Grid and tilemap

--- Scenes/Entities/Fawn/fawn.gd
+++ Scenes/Entities/Fawn/fawn.gd
@@ -1,1 +1,5 @@
 extends Node2D
+
+func _ready():
+    position = position.snapped(Vector2.ONE * Constants.tile_size)
+    position -= Vector2.ONE * Constants.tile_size / 2
---
+++ Scenes/Scripts/Constants.gd
@@ -0,0 +1,4 @@
+extends Node
+
+#size of the tiles in the tilemap
+var tile_size: int = 16
