05af2ffd2139cd292d2088a7ba87dd792582a0c7
Arespi <arespigames@gmail.com>
2023-11-02 21:30:10 +0800

    Implement change in sprites when fawn is moving

--- Scenes/Components/PathfindingAgent/pathfinding_agent.gd
+++ Scenes/Components/PathfindingAgent/pathfinding_agent.gd
@@ -5,6 +5,9 @@ var tilemap: TileMap
 var path = [] 
 
 var moving: bool = false
+var is_facing_right: bool = true
+
+signal agent_moving(is_moving: bool, facing_right: bool)
 
 func _ready():
 	pass
@@ -18,14 +21,19 @@ func _input(event):
 
 	path = Constants.astar_grid.get_id_path(
 			tilemap.local_to_map(get_owner().global_position),
-			tilemap.local_to_map(get_owner().get_global_mouse_position())).slice(1)
+			tilemap.local_to_map(get_owner().get_global_mouse_position()))
 
 	moving = true
+
+	var current_point = path.pop_front()
+	set_agent_moving(current_point)
+
 	print(path)
 
 func _process(_delta):
 	if path.is_empty():
 		moving = false
+		agent_moving.emit(false, is_facing_right)
 		return
 
 	var next_position = tilemap.map_to_local(path.front())
@@ -33,4 +41,13 @@ func _process(_delta):
 	get_owner().global_position = get_owner().global_position.move_toward(next_position, 1)
 
 	if get_owner().global_position == next_position:
-		path.pop_front()
+		var current_point = path.pop_front()
+
+		if not path.is_empty():
+			set_agent_moving(current_point)
+
+func set_agent_moving(current_point: Vector2):
+	if current_point.x != path.front().x:
+		is_facing_right = current_point.x < path.front().x
+		
+	agent_moving.emit(true, is_facing_right)
--- Scenes/Entities/Fawn/fawn.gd
+++ Scenes/Entities/Fawn/fawn.gd
@@ -3,5 +3,16 @@ extends Node2D
 @export var tilemap: TileMap
 
 func _ready():
-    position = Util.snap_to_grid(position)
-    $PathfindingAgent.tilemap = tilemap
+	position = Util.snap_to_grid(position)
+	$PathfindingAgent.tilemap = tilemap
+
+func _on_pathfinding_agent_agent_moving(is_moving: bool, facing_right: bool):
+	if is_moving:
+		$AnimatedSprite2D.play("Fawn_Walking")
+	else:
+		$AnimatedSprite2D.play("Fawn_IDLE")
+
+	if facing_right:
+		$AnimatedSprite2D.flip_h = false
+	else:
+		$AnimatedSprite2D.flip_h = true
